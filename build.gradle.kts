fda0905de93f048ec2caceaff0388f5a0d42db46
Saurabh Sandav <justmailsaurabh@gmail.com>
2022-02-02 13:37:29 +0530

    Add Kotlin Binary compatibility validator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.gradle.versions.checker)
+    alias(libs.plugins.kotlinx.binaryCompatibilityValidator)
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.plugin.serialization) apply false
