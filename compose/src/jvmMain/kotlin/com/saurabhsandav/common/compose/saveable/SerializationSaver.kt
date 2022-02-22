e9e74e473e21d5e6d966ba01a050cfb5a30b2c87
Saurabh Sandav <justmailsaurabh@gmail.com>
2022-02-22 12:26:53 +0530

    Desktop: Replace serializableSaver() implementation with autoSaver()

--- compose/src/jvmMain/kotlin/com/saurabhsandav/common/compose/saveable/SerializationSaver.kt
+++ compose/src/jvmMain/kotlin/com/saurabhsandav/common/compose/saveable/SerializationSaver.kt
@@ -1,14 +1,9 @@
 package com.saurabhsandav.common.compose.saveable
 
 import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.autoSaver
 import kotlinx.serialization.KSerializer
 
 public actual fun <T : Any> serializableSaver(
     serializer: KSerializer<T>,
-): Saver<T, Any> = Saver(
-    save = { it },
-    restore = {
-        @Suppress("UNCHECKED_CAST")
-        it as T
-    },
-)
+): Saver<T, Any> = autoSaver()
